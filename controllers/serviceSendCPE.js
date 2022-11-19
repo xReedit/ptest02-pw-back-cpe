d67c702feaa4b64e5e0c2a3bc6216d2ec0e32ec7
Marcelo <macraze.info@gmail.com>
2022-11-19 03:09:26 -0500

    new comprobacion

--- controllers/serviceSendCPE.js
+++ controllers/serviceSendCPE.js
@@ -53,7 +53,7 @@ function loop_process_validacion() {
 
 	console.log('ingresa a loops')
 	// todos los dias en el minuto 1 pasada las 1,3,5hrs corre proceso validacion api sunat
-	cron.schedule('8 1,3,5 * * *', () => {		
+	cron.schedule('10 1,3,5 * * *', () => {		
 		console.log('Cocinando validacion en api sunat ', date_now.toLocaleDateString());			
 		runCPEApiSunat()	  	
 	});
